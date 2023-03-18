65e827fb369c485e47156ff2ff7ef859243cfb60
Juliana Martinez Gomez <Juliana.martinez@sinco.com.co>
2023-03-18 02:00:22 -0500

    v2-taller algoritmos

--- algoritmo.js
+++ algoritmo.js
@@ -1,11 +1,92 @@
+//Solicitar nombre e imprimirlo
+let nombre;
 
+nombre=prompt("Escriba su nombre");
 
+console.log("El nombre ingresado es: "+ nombre);    
+//////////////////////////////////////////////////////////////
+//Calcular area y y perimetro de un rectangulo
 function Calcular(base,altura){
     const area=base*altura;
     const perimetro=2*(base*altura);
 
     return [area,perimetro];
 }
+//////////////////////////////////////////////////////////////////
+//Calcular la hipotenusa de triangulo rectangulo
+function CalcularHipotenusa(a,b){
+    const hipotenusa=Math.sqrt(a**2+b**2);
+    return hipotenusa;
+
+}
 
 let resultado= Calcular(5,9);
-console.log(resultado[0]);  
+console.log("El area del rectangulo es: "+resultado[0]);  
+let resultadohipo=CalcularHipotenusa(10,20);
+
+///////////////////////////////////////////////////////////////////
+//Suma,resta,división y multiplicacion de dos números
+
+
+function suma (a,b){
+    return a+b;
+}
+function resta(a,b){
+    return a-b;
+}
+function multiplicacion(a,b)
+{
+    return a*b;
+}
+function division(a,b)
+{
+    return a/b;
+}
+
+let resultadoSuma=suma(2,3);
+let resultadoResta=resta(10,5);
+let resultadoMultiplicacion=multiplicacion(10,9);
+let resultadoDivision=division(30,5);
+
+console.log("Resultado suma: "+resultadoSuma);
+console.log("Resultado resta: "+resultadoResta);
+console.log("Resultado multiplicacion: "+resultadoMultiplicacion);
+console.log("Resultado división: "+resultadoDivision);
+
+/////////////////////////////////////////////////////////////////////////
+//Media de 3 números
+let numero1,numero2,numero3;
+
+
+numero1=prompt("Digite el primer número");
+
+numero2=prompt("Digite el segundo número");
+
+numero3=prompt("Digite el tercer número");
+
+let media=(numero1+numero2+numero3)/3;
+
+
+///////////////////////////////////////////////////////////////////
+//Promedio calificaciones
+var parcial1,  parcial2,parcial3,examenfinal,trabajofinal;
+
+parcial1=prompt("Nota primer parcial");
+
+parcial2=prompt("Nota segundo parcial");
+
+parcial3=prompt("Nota tercer parcial");
+
+examenfinal=prompt("Nota examen final");
+
+trabajofinal=prompt("Nota trabajo final");
+
+var notaparcial=(parcial1+parcial2+parcial3)/3;
+
+var notafinal=(notaparcial*0,55)+(examenfinal*0,3)+(trabajofinal*0,15);
+
+console.log("nota final "+notafinal);
+
+///////////////////////////////////////////////////////////////7
+//Tiempo
+
